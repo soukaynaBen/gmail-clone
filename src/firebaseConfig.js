4f889713b5218c5931e8724793948e71355bf4b0
soukaynaBen <49325238+soukaynaBen@users.noreply.github.com>
2021-08-17 22:03:42 +0100

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,12 +1,12 @@
  import firebase from 'firebase'
  
  var firebaseConfig = {
-    apiKey: "AIzaSyDmlgoxaJK3iJQeTFfJMwWWWq7HSsCZNkA",
-    authDomain: "whatsapp-2-7bc96.firebaseapp.com",
-    projectId: "whatsapp-2-7bc96",
-    storageBucket: "whatsapp-2-7bc96.appspot.com",
-    messagingSenderId: "1078322551177",
-    appId: "1:1078322551177:web:376e4d9576be5408292958"
+    apiKey: "***************************************",
+    authDomain: "********************************",
+    projectId: "***************",
+    storageBucket: "*******************",
+    messagingSenderId: "**********",
+    appId: "********************************"
   };
   // Initialize Firebase
   const app=firebase.initializeApp(firebaseConfig);
